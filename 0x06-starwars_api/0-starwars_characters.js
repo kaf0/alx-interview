7c78f74dcb8764878c6e17a40dab10cc3a9c09d6
“kaf0” <gertrudeseworh@gmail.com>
2023-08-09 19:17:30 -0700

    Done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,39 @@
-#!/usr/bin/node
+const axios = require('axios');
 
-const request = require('request');
+function getMovieCharacters(movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  
+  return axios.get(url)
+    .then(response => {
+      const characterUrls = response.data.characters;
+      const characterPromises = characterUrls.map(characterUrl => axios.get(characterUrl));
+      
+      return Promise.all(characterPromises)
+        .then(characterResponses => {
+          const characterNames = characterResponses.map(characterResponse => characterResponse.data.name);
+          return characterNames;
+        })
+        .catch(error => {
+          console.error("Error fetching character data:", error);
+          return [];
+        });
+    })
+    .catch(error => {
+      console.error("Error fetching movie data:", error);
+      return [];
+    });
+}
+
+if (process.argv.length !== 3) {
+  console.error("Usage: node 0-starwars_characters.js <movie_id>");
+  process.exit(1);
+}
 
 const movieId = process.argv[2];
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-let characters = [];
-
-request(movieUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    characters = JSON.parse(body).characters;
-    getCharacter(0);
-  }
-});
-
-const getCharacter = (index) => {
-  if (index === characters.length) return;
-  request(characters[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const name = JSON.parse(body).name;
+getMovieCharacters(movieId)
+  .then(characterNames => {
+    characterNames.forEach(name => {
       console.log(name);
-      getCharacter(index + 1);
-    }
+    });
   });
-};
+
